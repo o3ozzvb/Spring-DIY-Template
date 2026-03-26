619414c1541d8f48a9c5333e941735aec461b0a9
oeunkyoung <o3ozzvb@gmail.com>
2026-03-26 22:29:24 +0900

    test(reflection): Reflection 학습 테스트 추가
    - Car 클래스 및 ReflectionTest를 study.reflection 패키지로 구성
    - 클래스 정보 조회, 메서드 실행 테스트 포함

---
+++ src/test/java/study/reflection/ReflectionTest.java
@@ -0,0 +1,42 @@
+package study.reflection;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+
+public class ReflectionTest {
+    private static final Logger log = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    @DisplayName("study.reflection.Car 객체 정보 가져오기")
+    void showClass() {
+        Class<Car> carClass = Car.class;
+        log.debug(carClass.getName());
+    }
+
+    @Test
+    void testMethodRun() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Car> carClass = Car.class;
+        Car car = carClass.getDeclaredConstructor(String.class, int.class).newInstance("Lia's Car", 5000);
+
+        Method[] declaredMethods = carClass.getDeclaredMethods();
+        for (Method method: declaredMethods) {
+            if (method.getName().startsWith("test")) {
+                // carClass.newInstance(); -> 'newInstance()' is deprecated since version 9
+                log.debug(method.invoke(car).toString());
+            }
+        }
+    }
+
+    @Test
+    void testAnnotationMethodRun() {
+
+    }
+
+}
